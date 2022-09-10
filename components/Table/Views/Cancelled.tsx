de188f14ed2e0ba7fbf909f9ee4af9ddb4148b69
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-09-10 19:29:54 +0100

    added missing sorted tables

--- components/Table/Views/Cancelled.tsx
+++ components/Table/Views/Cancelled.tsx
@@ -1,91 +1,146 @@
 import Link from 'next/link';
+import {
+  useSortableData,
+  getClassNamesFor,
+} from '../../../components/TableSorter/TableSorter';
 
-type Props = { orders: any; workTasks: any };
+const CancelledTable = (props: any) => {
+  const orders = props.orders;
+  const workTasks = props.workTasks;
 
-const CancelledTable: React.FunctionComponent<Props> = ({
-  orders,
-  workTasks,
-}) => (
-  <table className="min-w-max w-full table-auto">
-    <thead>
-      <tr className="bg-red-200 text-gray-600 uppercase text-sm leading-normal">
-        <th className="py-3 px-6 text-left">ID</th>
-        <th className="py-3 px-6 text-left">Start Date</th>
-        <th className="py-3 px-6 text-left">Brand Entry</th>
-        <th className="py-3 px-6 text-left">Work Task</th>
-        <th className="py-3 px-6 text-left">Decline Reason</th>
-        <th className="py-3 px-6 text-left">See All</th>
-      </tr>
-    </thead>
-    <tbody className="text-gray-600 text-sm font-light">
-      {orders
-        ? orders.map((order: any) => {
-            return (
-              <tr
-                key={order.tracking_id}
-                className="border-b border-gray-200 hover:bg-gray-100"
-              >
-                <td className="py-3 px-6 text-center">
-                  <div className="flex align-center items-center">
-                    <span className="text-center">
-                      {order.tracking_id}
-                    </span>
-                  </div>
-                </td>
-                <td className="py-3 px-6 text-center">
-                  <div className="flex align-center items-center">
-                    <span className="text-center">
-                      {order.start_time
-                        ? String(
-                            order.start_time
-                              .slice(0, 19)
-                              .replace(/T/g, ' ')
-                          )
-                        : null}
-                    </span>
-                  </div>
-                </td>
-                <td className="py-3 px-6 text-center">
-                  <div className="flex align-center items-center">
-                    <span className="text-center">
-                      {order.brand_entry}
-                    </span>
-                  </div>
-                </td>
-                <td className="py-3 px-6 text-center">
-                  <div className="flex align-center items-center">
-                    <span className="text-center">
-                      {
-                        workTasks.find(
-                          (task: any) =>
-                            task.id === order.work_task_id
-                        )?.name
-                      }
-                    </span>
-                  </div>
-                </td>
-                <td className="py-3 px-6 text-center">
-                  <div className="flex align-center items-center">
-                    <span className="text-center">
-                      {order.decline_reason}
-                    </span>
-                  </div>
-                </td>
-                <td>
-                  <div className="flex justify-center">
-                    <Link href={`/cancelled/${order.id}`}>
-                      <button className=" bg-blue-600 w-full rounded-md text-white outline-none focus:ring-4 shadow-lg">
-                        {'All Details'}
-                      </button>
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            );
-          })
-        : null}
-    </tbody>
-  </table>
-);
-
+  const { items, requestSort, sortConfig } = useSortableData(orders);
+  return (
+    <table className="min-w-max w-full table-auto">
+      <thead>
+        <tr
+          key={orders.tracking_id}
+          className="bg-red-200 text-gray-600 uppercase text-sm leading-normal"
+        >
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('tracking_id')}
+              className={getClassNamesFor('tracking_id', sortConfig)}
+            >
+              ID
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('start_time')}
+              className={getClassNamesFor('start_time', sortConfig)}
+            >
+              Start Date
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('brand_entry')}
+              className={getClassNamesFor('brand_entry', sortConfig)}
+            >
+              Brand Entry
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('work_task_id')}
+              className={getClassNamesFor('work_task_id', sortConfig)}
+            >
+              Work Task
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('decline_reason')}
+              className={getClassNamesFor(
+                'decline_reason',
+                sortConfig
+              )}
+            >
+              Decline Reason
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">See All</th>
+        </tr>
+      </thead>
+      <tbody className="text-gray-600 text-sm font-light">
+        {orders
+          ? items.map((order: any) => {
+              return (
+                <tr
+                  key={order.tracking_id}
+                  className="border-b border-gray-200 hover:bg-gray-100"
+                >
+                  <td className="py-3 px-6 text-center">
+                    <div className="flex align-center items-center">
+                      <span className="text-center">
+                        {order.tracking_id}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-6 text-center">
+                    <div className="flex align-center items-center">
+                      <span className="text-center">
+                        {order.start_time
+                          ? String(
+                              order.start_time
+                                .slice(0, 19)
+                                .replace(/T/g, ' ')
+                            )
+                          : null}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-6 text-center">
+                    <div className="flex align-center items-center">
+                      <span className="text-center">
+                        {order.brand_entry}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-6 text-center">
+                    <div className="flex align-center items-center">
+                      <span className="text-center">
+                        {
+                          workTasks.find(
+                            (task: any) =>
+                              task.id === order.work_task_id
+                          )?.name
+                        }
+                      </span>
+                    </div>
+                  </td>
+                  <td className="py-3 px-6 text-center">
+                    <div className="flex align-center items-center">
+                      <span className="text-center">
+                        {order.decline_reason}
+                      </span>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex justify-center">
+                      <Link href={`/cancelled/${order.id}`}>
+                        <button className=" bg-blue-600 w-full rounded-md text-white outline-none focus:ring-4 shadow-lg">
+                          {'All Details'}
+                        </button>
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          : null}
+      </tbody>
+    </table>
+  );
+};
 export default CancelledTable;
--- components/Table/Views/Completed.tsx
+++ components/Table/Views/Completed.tsx
@@ -1,94 +1,156 @@
-// import { PDFDownloadLink } from '@react-pdf/renderer/lib/react-pdf.browser.cjs.js';
-// import Receipt from '../../Receipt';
 import Link from 'next/link';
+import {
+  useSortableData,
+  getClassNamesFor,
+} from '../../../components/TableSorter/TableSorter';
 
-type Props = { orders: any; workTasks: any; brands: any };
-const CompletedTable: React.FunctionComponent<Props> = ({
-  orders,
-  workTasks,
-  brands,
-}) => (
-  <>
-    {orders && workTasks && brands ? (
-      <table className="min-w-max w-full table-auto">
-        <thead>
-          <tr className="bg-green-200 text-gray-600 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">ID</th>
-            <th className="py-3 px-6 text-left">Finish Date</th>
-            <th className="py-3 px-6 text-left">Work Task</th>
-            <th className="py-3 px-6 text-left">Brand</th>
-            <th className="py-3 px-6 text-left">
+const CompletedTable = (props: any) => {
+  const orders = props.orders;
+  const workTasks = props.workTasks;
+  const brands = props.brands;
+
+  const { items, requestSort, sortConfig } = useSortableData(orders);
+
+  return (
+    <table className="min-w-max w-full table-auto">
+      <thead>
+        <tr
+          key={orders.tracking_id}
+          className="bg-green-200 text-gray-600 uppercase text-sm leading-normal"
+        >
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('tracking_id')}
+              className={getClassNamesFor('tracking_id', sortConfig)}
+            >
+              ID
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('start_time')}
+              className={getClassNamesFor('start_time', sortConfig)}
+            >
+              Finish Date
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('work_task_id')}
+              className={getClassNamesFor('work_task_id', sortConfig)}
+            >
+              Work Task
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('brand_id')}
+              className={getClassNamesFor('brand_id', sortConfig)}
+            >
+              Brand
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('final_units_or_quantity')}
+              className={getClassNamesFor(
+                'final_units_or_quantity',
+                sortConfig
+              )}
+            >
               Final Units / Quantity
-            </th>
-            <th className="py-3 px-6 text-left">Time Taken</th>
-            {/* <th className="py-3 px-6 text-center">Receipt Link</th> */}
-            <th className="py-3 px-6 text-center">See All Details</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {orders ? (
-            orders.map((order: any) => {
-              return (
-                <tr
-                  key={order.tracking_id}
-                  className="border-b border-gray-200 hover:bg-gray-100"
-                >
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">
-                        {order.tracking_id}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">
-                        {order.finish_date
-                          ? String(
-                              order.finish_date
-                                .slice(0, 19)
-                                .replace(/T/g, ' ')
-                            )
-                          : null}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">
-                        {
-                          workTasks.find(
-                            (task: any) =>
-                              task.id === order.work_task_id
-                          )?.name
-                        }
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">
-                        {
-                          brands.find(
-                            (brand: any) =>
-                              brand.id === order.brand_id
-                          )?.name
-                        }
-                      </span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">{`${order.final_units_or_quantity}`}</span>
-                    </div>
-                  </td>
-                  <td className="py-3 px-6 text-center">
-                    <div className="flex align-center items-center">
-                      <span className="text-center">{`${order.minutes_taken} mins`}</span>
-                    </div>
-                  </td>
+            </button>
+          </th>
+          <th className="py-3 px-6 text-left">
+            <button
+              type="button"
+              id="sortButton"
+              onClick={() => requestSort('minutes_taken')}
+              className={getClassNamesFor(
+                'minutes_taken',
+                sortConfig
+              )}
+            >
+              Time Taken
+            </button>
+          </th>
+          {/* <th className="py-3 px-6 text-center">Receipt Link</th> */}
+          <th className="py-3 px-6 text-center">See All Details</th>
+        </tr>
+      </thead>
+      <tbody className="text-gray-600 text-sm font-light">
+        {orders ? (
+          items.map((order: any) => {
+            return (
+              <tr
+                key={order.tracking_id}
+                className="border-b border-gray-200 hover:bg-gray-100"
+              >
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">
+                      {order.tracking_id}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">
+                      {order.finish_date
+                        ? String(
+                            order.finish_date
+                              .slice(0, 19)
+                              .replace(/T/g, ' ')
+                          )
+                        : null}
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">
+                      {
+                        workTasks.find(
+                          (task: any) =>
+                            task.id === order.work_task_id
+                        )?.name
+                      }
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">
+                      {
+                        brands.find(
+                          (brand: any) => brand.id === order.brand_id
+                        )?.name
+                      }
+                    </span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">{`${order.final_units_or_quantity}`}</span>
+                  </div>
+                </td>
+                <td className="py-3 px-6 text-center">
+                  <div className="flex align-center items-center">
+                    <span className="text-center">{`${order.minutes_taken} mins`}</span>
+                  </div>
+                </td>
 
-                  {/* <td>
+                {/* <td>
                   <div className="flex justify-center bg-blue-600 rounded-md ">
                     <PDFDownloadLink
                       document={
@@ -109,25 +171,24 @@ const CompletedTable: React.FunctionComponent<Props> = ({
                     </PDFDownloadLink>
                   </div>
                 </td> */}
-                  <td>
-                    <div className="flex justify-center">
-                      <Link href={`/completed/${order.id}`}>
-                        <button className=" bg-blue-600 w-full rounded-md text-white outline-none focus:ring-4 shadow-lg">
-                          {'All Details'}
-                        </button>
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <p>Loading...</p>
-          )}
-        </tbody>
-      </table>
-    ) : null}
-  </>
-);
+                <td>
+                  <div className="flex justify-center">
+                    <Link href={`/completed/${order.id}`}>
+                      <button className=" bg-blue-600 w-full rounded-md text-white outline-none focus:ring-4 shadow-lg">
+                        {'All Details'}
+                      </button>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </tbody>
+    </table>
+  );
+};
 
 export default CompletedTable;
