199dbba782265bfab42076572b919479edde9dc7
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-09-18 19:26:58 +0100

    urlFix4

--- pages/api/zendesk/auth.js
+++ pages/api/zendesk/auth.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
   response.redirect(
-    `${process.env.NEXT_PUBLIC_ZENDESK_URL}/oauth/authorizations/new?response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/api/zendesk/callback&client_id=${NEXT_PUBLIC_ZENDESK_CLIENT_ID}&scope=read%20write`
+    `${process.env.NEXT_PUBLIC_ZENDESK_URL}/oauth/authorizations/new?response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_HOMEPAGE_URL}/api/zendesk/callback&client_id=${process.env.NEXT_PUBLIC_ZENDESK_CLIENT_ID}&scope=read%20write`
   );
 }
