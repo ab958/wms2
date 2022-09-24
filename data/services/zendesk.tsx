f534575d910af28123034215b9bb222519e56803
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-09-24 15:01:55 +0100

    update zendesk ticket

--- data/services/zendesk.tsx
+++ data/services/zendesk.tsx
@@ -1,51 +1,28 @@
-import axios from "axios";
+import axios from 'axios';
 
-
-export const createZendeskTicket = async (ticketData: object) => {
+export const updateZendeskTicket = async (
+  ticketID: number,
+  ticketData: object
+) => {
   try {
     const headers = {
-      // 'Content-Type': 'application/json',
-      Authorization: `Bearer ${window.localStorage.getItem("url")}`,
-      "Access-Control-Allow-Origin": "*"
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${window.localStorage.getItem('url')}`,
+      // "Access-Control-Allow-Origin": "*"
     };
 
-    let body ={
-      headers,
-      ticketData
-    }
-
-    const {data} = await axios.post('/api/zendex/createticket', body)
-
-    console.log(data.response,"bbb")
-    return {success: true, data};
-  } catch (error) {
-    console.log(error);
-    return {success: false};
-  }
-};
-
-
-export const updateZendeskTicket = async (ticketID: number, ticketData: object) => {
-
-  try {
-    const headers = {
-      // 'Content-Type': 'application/json',
-      Authorization: `Bearer ${window.localStorage.getItem("url")}`,
-      "Access-Control-Allow-Origin": "*"
-    };
-
-    let body ={
-      headers,
+    const { data } = await axios.put(
+      `${process.env.NEXT_PUBLIC_ZENDESK_URL}/api/v2/tickets/${ticketID}.json`,
       ticketData,
-      ticketID
-    }
-
-    const {data} = await axios.post('/api/zendex/updateticket', body)
-    console.log(data,"aaaaa")
-    return {success: true, data};
+      {
+        headers,
+      }
+    );
+    console.log(data, 'aaaaa');
+    return { success: true, data };
   } catch (error) {
+    console.log('error');
     console.log(error);
-    return {success: false};
+    return { success: false };
   }
 };
-
